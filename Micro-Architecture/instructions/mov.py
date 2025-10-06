9ab883c5e9f266d94c8ad3ea3106af9f56f8808a
f4bms <fmelendezsequeira@gmail.com>
2025-10-06 00:28:18 -0600

    Added Fixed Instructions

--- Micro-Architecture/instructions/mov.py
+++ Micro-Architecture/instructions/mov.py
@@ -1,43 +1,36 @@
 #mov
 
 class Mov:
-    def __init__(self, _destino,  _inmediate, _procesador):
+    def __init__(self, _destino, _fuente, _procesador):
         self.destino = _destino
-        self.inmediate = _inmediate
+        self.fuente = _fuente
         self.procesador = _procesador
-
-        self.ejecucion = [self.instruccion1, self.instruccion2, self.instruccion3]
-
-    def instruccion1(self):
-        #print("Obteniendo de registro "+str(self.registro1))
-        
-        
-        #self.procesador.regRF.data = self.procesador.RF.registros[self.registro1]
-        
-        #print(self.procesador.regRF.data)
-        pass
-
-    def instruccion2(self):
-        #print("Sumando ")
-        #self.procesador.regALU.data = self.procesador.ALU.operar(self.procesador.regRF.data, self.inmediate, 0)
-        #print(self.procesador.regALU.data)
-        pass
-
-    def instruccion3(self):
-        print("Guardando inmediato en registro")
-        self.procesador.RF.registros[self.destino] = self.inmediate
-        print(str(self.procesador.RF.registros[self.destino]) + " en: " + str(self.destino))
-
-
-        
-
+        self.ejecucion = [self.decode, self.execute, self.memory, self.writeback]
+    
+    def decode(self):
+        print(f"Leyendo registro fuente R{self.fuente}")
+        self.procesador.regRF.data = self.procesador.RF.registros[self.fuente]
+        print(f" Valor leído: {self.procesador.regRF.data}")
+    
+    def execute(self):
+        self.procesador.regALU.data = self.procesador.ALU.operar(self.procesador.regRF.data, 0, 0)
+        print(f" Valor en ALU: {self.procesador.regALU.data}")
+    
+    def memory(self):
+        self.procesador.regDM.data = self.procesador.regALU.data
+        print(f" Valor: {self.procesador.regDM.data}")
+    
+    def writeback(self):
+        print(f"Escribiendo en R{self.destino}")
+        self.procesador.RF.registros[self.destino] = self.procesador.regDM.data
+        print(f"R{self.destino} = {self.procesador.RF.registros[self.destino]}")
+    
     def ejecutar(self):
         if self.ejecucion:
             fase = self.ejecucion.pop(0)
             fase()
         else:
             print("No hay más fases para ejecutar en Mov.")
-
 
 
 
