d2ad356a48620cb7b0ba7a958bba6fea67f06e39
Karina Martinez <karinamguerrero@estudiantec.cr>
2025-10-08 21:14:02 -0600

    [Update] modify names

--- Micro-Architecture/components/vault.py
+++ Micro-Architecture/components/vault.py
@@ -1,20 +1,32 @@
-class Boveda:
-    def init(self):
-        self.llaves = [0]*4  # cada una de 64 bits
-        self.hashes_iniciales = [0]*4  # A, B, C, D
+class vault:
+    def __init__(self):
+        # Llaves privadas (solo lectura/escritura segura)
+        self.keys = [0]*4  # cada una de 64 bits
+        # Valores iniciales de hash
+        self.hashes = [0]*4  # A, B, C, D
 
-    def escribir_llave(self, indice, valor):
-        if 0 <= indice < 4:
-            self.llaves[indice] = valor
+    # Escritura de llaves o hashes desde instrucciones especiales
+    def write_key(self, index, valor):
+        if 0 <= id < 4:
+            self.keys[index] = valor
 
-    def escribir_hash(self, indice, valor):
-        if 0 <= indice < 4:
-            self.hashes_iniciales[indice] = valor
+    def write_hash(self, index, valor):
+        if 0 <= index < 4:
+            self.hashes[index] = valor
 
-    def leer_llave(self, indice):
-        return self.llaves[indice]
+    # Lectura controlada (solo por CPU)
+    def compare_llave(self, index):
+        return self.keys[index]
 
-    def leer_hash(self, indice):
-        return self.hashes_iniciales[indice]
+    def compare_hash(self, index):
+        return self.hashes[index]
     
-    #
+#Comparar hash   CHLL rd, rs1, index (Registro para resultado, registro a comparar, indice del hash)
+#Guardar hash    GRDLL rd, rs1, index
+#Comparar llave  CHH
+#Guardar llave   GRDH
+    
+    #Login y tenemos contraseña en boveda, me compara la que le doy con la hardcoded en boveda
+    # Esto activa un registro interno para comprobar si se logueó o no
+    # Si no está logueado, las de modificar no modifican y las de cargar me devuleven 0
+    
